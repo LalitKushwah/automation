7572226f8604f536c5c38ec15ee3e551bd2a64ce
LalitKushwah <lalitkushwah52525@gmail.com>
2021-09-17 20:31:52 +0530

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 // const path = require('path');
 
 app.get('/health', (req, res) => {
-    res.send({ message: 'You Did it' })
+    res.send({ message: 'You Did it!!' })
 });
 
 app.listen(3000, () => {
